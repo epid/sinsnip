f49a358de9a278dd83ceae0f613c8f42f416b213
Jared Giles <jared@aleph-one.net>
2009-01-30 09:46:40 -0600

    Added the snippet model

---
+++ lib/snippet.rb
@@ -0,0 +1,33 @@
+class Snippet < Sequel::Model
+
+  # Validations
+  validates_presence_of :title, :body, :language
+
+  # Hooks
+  before_create do
+    self.created_at = Time.now
+    self.updated_at = Time.now
+  end
+
+  unless table_exists?
+    set_schema do
+      primary_key :id
+      string      :title
+      text        :body
+      string      :language
+      integer     :views, :default => 0
+      datetime    :created_at
+      datetime    :updated_at
+    end
+    create_table
+  end
+  
+  def self.most_recent( n = 3 )
+    reverse_order(:updated_at).limit( n )
+  end
+
+  def self.most_viewed( n = 3 )
+    order(:views.desc).limit( n )
+  end
+
+end
--- sinsnip.rb
+++ sinsnip.rb
@@ -2,8 +2,22 @@
 
 require 'rubygems'
 require 'sinatra'
+require 'sequel'
+
+configure do
+  Sequel.connect('sqlite://snippets.db')
+end
+
+$LOAD_PATH.unshift(File.dirname(__FILE__) + '/lib')
+require 'snippet'
+
+before do
+  @most_recent = Snippet.most_recent
+  @most_viewed = Snippet.most_viewed
+end
 
 get '/' do
+  @snippets = Snippet.all
   haml :index
 end
 
